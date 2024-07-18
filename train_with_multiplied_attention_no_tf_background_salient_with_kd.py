2ea36675763b4b31aae30adfc8ab02c90ebc767e
unknown <abrar.bd27@gmail.com>
2024-07-18 14:30:44 +0600

    updated data.yaml file

--- train_with_multiplied_attention_no_tf_background_salient_with_kd.py
+++ train_with_multiplied_attention_no_tf_background_salient_with_kd.py
@@ -73,6 +73,13 @@ from utils.plots import plot_evolve
 from utils.torch_utils import (EarlyStopping, ModelEMA, de_parallel, select_device, smart_DDP, smart_optimizer,
                                smart_resume, torch_distributed_zero_first)
 
+## for windows only
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+
 LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable/elastic/run.html
 RANK = int(os.getenv('RANK', -1))
 WORLD_SIZE = int(os.getenv('WORLD_SIZE', 1))
