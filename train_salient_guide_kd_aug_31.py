e654d37d195f91e807281f3532e74c35dda80749
unknown <abrar.bd27@gmail.com>
2024-09-04 02:16:19 +0600

    changed for colab

--- train_salient_guide_kd_aug_31.py
+++ train_salient_guide_kd_aug_31.py
@@ -448,7 +448,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     maps = np.zeros(nc)  # mAP per class
     results = (0, 0, 0, 0, 0, 0, 0)  # P, R, mAP@.5, mAP@.5-.95, val_loss(box, obj, cls)
     scheduler.last_epoch = start_epoch - 1  # do not move
-    scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    scaler = torch.amp.GradScaler(enabled=amp)
     stopper, stop = EarlyStopping(patience=opt.patience), False
     getloss = SelfDistLoss(model,teacher_model,device)
     
