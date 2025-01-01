310ddb2d2e769f3b7610918ed9ae6796bede0b74
unknown <abrar.bd27@gmail.com>
2025-01-02 01:34:44 +0600

    changed on 2 jan 2025

--- train.py
+++ train.py
@@ -39,9 +39,9 @@ from torch.optim import lr_scheduler
 from tqdm import tqdm
 
 ## for windows only
-import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+# import pathlib
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
