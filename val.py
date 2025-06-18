be387e4db37430e2bfc8a828e22f72a21a81a35b
Md. Abrar Zahin <60336583+AbrarZahin247@users.noreply.github.com>
2025-06-18 19:37:23 +0600

    Update val.py

--- val.py
+++ val.py
@@ -40,7 +40,7 @@ from models.common import DetectMultiBackend
 from utils.callbacks import Callbacks
 from utils.dataloaders import create_dataloader
 from utils.general import (LOGGER, TQDM_BAR_FORMAT, Profile, check_dataset, check_img_size, check_requirements,
-                           check_yaml, coco80_to_coco91_class, colorstr, increment_path, non_max_suppression,
+                           check_yaml, coco80_to_coco91_class, check_amp, colorstr, increment_path, non_max_suppression,
                            print_args, scale_boxes, xywh2xyxy, xyxy2xywh)
 from utils.metrics import ConfusionMatrix, ap_per_class, box_iou
 from utils.plots import output_to_target, plot_images, plot_val_study
