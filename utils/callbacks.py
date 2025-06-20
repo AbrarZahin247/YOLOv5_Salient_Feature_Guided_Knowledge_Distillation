7a0156f434e4a6fccae748a6929f5a9d5c069245
Md. Abrar Zahin <60336583+AbrarZahin247@users.noreply.github.com>
2025-06-20 22:36:31 +0600

    Update callbacks.py

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,18 +1,18 @@
 # YOLOv5 🚀 by Ultralytics, AGPL-3.0 license
 """
-Callback utils
+Callback functions for YOLOv5 models
 """
 
-import threading
+import inspect
 
 
 class Callbacks:
-    """"
-    Handles all registered callbacks for YOLOv5 Hooks
+    """
+    Handles all YOLOv5 callback functions
     """
 
     def __init__(self):
-        # Define the available callbacks
+        # Define callback dictionary with all required hooks
         self._callbacks = {
             'on_pretrain_routine_start': [],
             'on_pretrain_routine_end': [],
@@ -28,49 +28,37 @@ class Callbacks:
             'on_val_image_end': [],
             'on_val_batch_end': [],
             'on_val_end': [],
-            'on_fit_epoch_end': [],  # fit = train + val
+            'on_fit_epoch_end': [],
             'on_model_save': [],
             'on_train_end': [],
             'on_params_update': [],
-            'teardown': [], }
-        self.stop_training = False  # set True to interrupt training
+            'check_dataset': [],
+            'process_preds': [],
+            'upload_dataset_to_wandb': [],  # <-- THE FINAL FIX
+            'teardown': []}
+        self.stop_training = False
 
     def register_action(self, hook, name='', callback=None):
         """
         Register a new action to a callback hook
-
-        Args:
-            hook: The callback hook name to register the action to
-            name: The name of the action for later reference
-            callback: The callback to fire
         """
-        assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
+        assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks.keys()}"
         assert callable(callback), f"callback '{callback}' is not callable"
         self._callbacks[hook].append({'name': name, 'callback': callback})
 
     def get_registered_actions(self, hook=None):
-        """"
-        Returns all the registered actions by callback hook
-
-        Args:
-            hook: The name of the hook to check, defaults to all
         """
-        return self._callbacks[hook] if hook else self._callbacks
-
-    def run(self, hook, *args, thread=False, **kwargs):
+        Returns all registered actions by hook
         """
-        Loop through the registered actions and fire all callbacks on main thread
+        return self._callbacks if hook is None else self._callbacks.get(hook, [])
 
-        Args:
-            hook: The name of the hook to check, defaults to all
-            args: Arguments to receive from YOLOv5
-            thread: (boolean) Run callbacks in daemon thread
-            kwargs: Keyword Arguments to receive from YOLOv5
+    def run(self, hook, *args, **kwargs):
         """
-
-        assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
+        Run all actions registered to a specific callback hook
+        """
         for logger in self._callbacks[hook]:
-            if thread:
-                threading.Thread(target=logger['callback'], args=args, kwargs=kwargs, daemon=True).start()
-            else:
-                logger['callback'](*args, **kwargs)
+            # Get logger arguments
+            logger_args = inspect.getfullargspec(logger['callback']).args
+            # Pass only accepted arguments
+            kwargs_ = {k: v for k, v in kwargs.items() if k in logger_args}
+            logger['callback'](*args, **kwargs_)
