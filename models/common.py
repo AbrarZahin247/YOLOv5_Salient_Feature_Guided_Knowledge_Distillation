d28f7ae53969651f2a626e2b07508d70a503b888
Md. Abrar Zahin <60336583+AbrarZahin247@users.noreply.github.com>
2025-06-20 22:58:25 +0600

    Update common.py

--- models/common.py
+++ models/common.py
@@ -36,7 +36,8 @@ except (ImportError, AssertionError):
 
 from ultralytics.utils.plotting import Annotator, colors, save_one_box
 
-from utils import TryExcept
+# from utils import TryExcept
+from utils.general import TryExcept
 from utils.dataloaders import exif_transpose, letterbox
 from utils.general import (
     LOGGER,
